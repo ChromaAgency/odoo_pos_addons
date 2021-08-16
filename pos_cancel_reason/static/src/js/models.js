dd0745bea8222bdeb28f40eb7c9afc2ce5c15ae7
Federico Gobea <fgobea@making.com.ar>
2021-08-16 08:31:53 -0300

    [rem] SENT NOT CORRESPONDING FEATURE TO ANOTHER MODULE

--- pos_cancel_reason/static/src/js/models.js
+++ pos_cancel_reason/static/src/js/models.js
@@ -5,19 +5,7 @@ odoo.define('pos_cancel_reason.models',
   var exports = require('point_of_sale.models');
   var _super_order = exports.Order.prototype
   exports.Order = exports.Order.extend({
-    initialize: function(attributes,options){
-      var order = _super_order.initialize.apply(this, arguments);
-      if (!this.client){
-        var client = this.pos.db.get_partner_by_id(7);
-        if (!client) {
-                  console.error('ERROR: trying to load a partner not available in the pos');
-              }
-        else{
-            this.set_client(client)
-          }
-      }
-      return order;
-    },
+
     export_as_JSON:function(){
         var json = _super_order.export_as_JSON.apply(this,arguments);
         json.cancel_reason = this.cancel_reason;
