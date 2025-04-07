1287ccf3aba0193534cef7d6f10670389d071b84
Federico Gobea <fgobea@chroma.agency>
2025-04-07 07:40:32 -0500

    added new alternative invoice journal

---
+++ pos_alt_invoice_journal/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'POS Alt Invoice Journal',
+    'version': '18.0.1.0.0',
+    'category': 'Point of Sale',
+    'summary': 'Change journal when invoice field is not set in POS',
+    'description': """
+        This module modifies the POS behavior to use a different journal when
+        the invoice field is not set during invoice creation.
+    """,
+    'depends': ['point_of_sale', 'parallel_accounting'],
+    'data': [
+    ],
+    'assets': {
+        'point_of_sale.assets': [
+        ],
+    },
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
